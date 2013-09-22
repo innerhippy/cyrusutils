cfee4b520c1863c48d8200232fd68b4104743674
Will Hall <will@innerhippy.com>
2013-09-22 19:49:49 +0000

    rename to cyrusutils

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import sys
 from setuptools import setup
 
-setup(name='cyrusmigrate',
+setup(name='cyrusutils',
 	version="1.0",
-	description='Utility to convert cyrus imap accounts',
-#	long_description=readme(),
-	url='http://github.com/innerhippy/pydosh',
+	description='Cyrus imap account conversion utilities',
+	url='http://github.com/innerhippy/cyrusutils',
 	author='Will Hall',
 	author_email='will@innerhippy.com',
 	license='GPLv3',
-	packages=['cyrusmigrate'],
+	packages=['cyrusutils'],
 	scripts=['scripts/cyrusmigrate'],
 	zip_safe=False,
 	test_suite='tests',
